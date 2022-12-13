11e99425c5eb9f845b06dc5ec5f5e313b3997dcc
yang zhou <xtuzhouyang@outlook.com>
2022-12-13 10:56:30 +0800

    添加项目文件。

---
+++ Veldrid.VK.iOSNativeAsset/libMoltenVK-ios.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith("libMoltenVK-ios.a", LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
---
+++ Veldrid.VK.macOSNativeAsset/libMoltenVK-macos.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith("libMoltenVK-macos.a", LinkTarget.Arm64 | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]

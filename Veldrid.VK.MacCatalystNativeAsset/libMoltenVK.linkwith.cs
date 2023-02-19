ebfffb4f449c3219e4ae1112e5a8319fbf3051db
xtuzy <1570571499@qq.com>
2023-02-19 14:54:39 +0800

    unify version number, moltenvk is 1.3.231, we use 1.3.2311, 1.3.2312... for fix binding bug

--- Veldrid.VK.MacCatalystNativeAsset/libMoltenVK.linkwith.cs
+++ Veldrid.VK.MacCatalystNativeAsset/libMoltenVK.linkwith.cs
@@ -1,4 +1,4 @@
 ﻿using System;
 using ObjCRuntime;
 
-[assembly: LinkWith("libMoltenVK.a", LinkTarget.Arm64, Frameworks="IOSurface QuartzCore Metal Foundation IOKit UIKit", SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith("libMoltenVK.a", LinkTarget.Arm64 | LinkTarget.Simulator64, Frameworks="IOSurface QuartzCore Metal Foundation IOKit UIKit", SmartLink = true, ForceLoad = true)]
